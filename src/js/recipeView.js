d23ab343fd0613868e08ab40a605886a381c7468
Abhyshekbhalaji <abhyshekbhalaji@gmail.com>
2024-05-04 20:05:13 +0530

    Changed fraction to numberto fraction

--- src/js/recipeView.js
+++ src/js/recipeView.js
@@ -1,5 +1,5 @@
 import icons from "url:../img/icons.svg";
- import {Fraction} from "fractional";
+import { numberToFraction } from '../helpers.js';
 import view from"./view.js"
 //  var fraction =require( 'fractional' );
  class RecipeView extends view{
@@ -65,7 +65,7 @@ _generateMarkupIngredient(ing){
     <svg class="recipe__icon">
       <use href="${icons}#icon-check"></use>
     </svg>
-    <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString():""}</div>
+    <div class="recipe__quantity">${ing.quantity ? numberToFraction(ing.quantity).toString():""}</div>
 
     <div class="recipe__description">
       <span class="recipe__unit">${ing.unit}</span>
